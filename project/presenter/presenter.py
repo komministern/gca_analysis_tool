4f81eac5efc2b7a0f77f02a1faa7596c05386580
Oscar Franzén <oscarfranzen@yahoo.se>
2017-11-26 12:44:53 +0100

    v1.29 again
    updated the About window text

--- project/presenter/presenter.py
+++ project/presenter/presenter.py
@@ -917,7 +917,7 @@ class MyPresenter(QtCore.QObject):
     def about(self):
 
         self.message(u'''
-GCA Analysis Tool, v1.19 Trial
+GCA Analysis Tool, v1.29 Trial
 
 Copyright © 2016, 2017 Oscar Franzén <oscarfranzen@protonmail.com>
 
